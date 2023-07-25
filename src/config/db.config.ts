2f2684adb613f29b7b44d855ed1c8ed49cfb345e
hyunwoo.jo <hyunwoo.jo@monitorcorp.ai>
2023-07-25 17:51:01 +0900

    Hotfix:

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -5,5 +5,5 @@ export default registerAs("db", () => ({
     port: parseInt(process.env.DB_PORT, 10) || 5432,
     name: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
   }));
